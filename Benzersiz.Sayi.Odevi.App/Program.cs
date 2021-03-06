e348eb0891f6c55b28a3d7b4332ceea01d95a234
Arif Uslu <atalha.uslu@gazi.edu.tr>
2021-03-06 15:22:38 +0300

    Add project files.

---
+++ Benzersiz.Sayi.Odevi.App/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benzersiz.Sayi.Odevi.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kaç tane 'Benzersiz Sayı' üretilecek?:");
+            int adet;
+            adet = int.Parse(Console.ReadLine());
+            int[] sayilar = new int[adet];
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                sayilar[i] = rnd.Next(0, 1000);
+            }
+            Array.Sort(sayilar);
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. Benzersiz sayınız: {sayilar[i]}");
+            }
+        }
+    }
+}
